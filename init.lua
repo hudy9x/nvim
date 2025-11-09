078b54b4c47af634a3e1d2512e90e3e153424b4c
hudy@mac-air <hudy9x@gmail.com>
2025-11-09 16:26:37 +0700

    add bufferline for displaying tabs

--- init.lua
+++ init.lua
@@ -57,8 +57,8 @@ require('lazy').setup({
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
+        add = { text = '┃' },
+        change = { text = '┃' },
         delete = { text = '_' },
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
@@ -134,6 +134,8 @@ require('lazy').setup({
   },
 
   require 'kickstart.plugins.blink',
+
+  require 'kickstart.plugins.bufferline',
 
   require 'themes.tokyonight',
 
--- lua/custom/map.lua
+++ lua/custom/map.lua
@@ -33,6 +33,7 @@ vim.keymap.set('n', '<c-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<c-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
+--
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
@@ -52,6 +53,15 @@ keymap.set('n', 'sv', ':vsplit<cr><C-w>w')
 -- open toggle term
 keymap.set('n', '<leader>t', '<cmd>ToggleTerm<CR>')
 keymap.set('t', '<esc>', [[<C-\><C-n>:ToggleTerm<CR>]])
+
+-- Next/Prev tab page
+keymap.set('n', '<leader>l', ':BufferLineCycleNext<CR>')
+keymap.set('n', '<leader>h', ':BufferLineCyclePrev<CR>')
+
+-- Close all tabs
+keymap.set('n', '<leader>ta', [[:tabedit<cr>:BufferLineCloseLeft<cr>:BufferLineCloseRight<cr>]])
+-- Keep current tab and close the others
+keymap.set('n', '<leader>to', [[:BufferLineCloseLeft<cr>:BufferLineCloseRight<cr>]])
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
